f1a9c87a1e635f13b373e9b14c9c75f27c15ab0c
lytpro-academy <lytpro.academy@gmail.com>
2024-04-16 20:45:41 +0200

    initial implementation of user_service

---
+++ user_service/src/main/java/com/user_service/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class UserController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello from user service";
+    }
+}
